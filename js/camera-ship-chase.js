46ce860e23a733cb8bbd662f96ee16fa6b505a21
Cristi Jugaru <jugarucristi@users.noreply.github.com>
2020-07-17 00:00:21 +0300

    Added collision support

--- js/camera-ship-chase.js
+++ js/camera-ship-chase.js
@@ -4,7 +4,7 @@ tick: function() {
   var shipEl=scene.querySelector('#ship');
 
   var pos=shipEl.getAttribute('position');
-  if (-this.el.object3D.rotation.y*2>-1.25 && -this.el.object3D.rotation.y*1.4<1.5){
+  if (-this.el.object3D.rotation.y*2>-1.25 && -this.el.object3D.rotation.y*1.4<1.5){ //restricts ship movement to game boundaries
     shipEl.setAttribute('position',{x: -this.el.object3D.rotation.y*1.4, y: pos.y, z: pos.z});
   }
 
